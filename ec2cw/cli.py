1b52bd85e77b1db2ac821ba206650cb0c5be1b3a
Robert Wikman <rbw@vault13.org>
2019-10-10 13:43:47 +0200

    ValueError on missing dimension value

--- ec2cw/cli.py
+++ ec2cw/cli.py
@@ -12,6 +12,9 @@ class DimensionParser(argparse.Action):
         kvs = {}
         for pair in values.split(','):
             k, v = pair.split('=')
+            if not v:
+                raise ValueError("Dimension {} value cannot be empty".format(k))
+
             kvs[k] = v
 
         setattr(namespace, self.dest, kvs)
