9db83315b31f6bbecdbc975b41173a4f11f0058c
Robert Wikman <rbw@vault13.org>
2019-10-10 13:58:42 +0200

    Discard result in ok and problem of Summary

--- ec2cw/summary.py
+++ ec2cw/summary.py
@@ -9,8 +9,8 @@ class CloudWatchSummary(nagiosplugin.Summary):
             # Convert dimensions back into str {x: y} => x=y for a better human experience.
             self._msg += " ({})".format(', '.join("{0}={1}".format(*d) for d in dimensions.items()))
 
-    def ok(self, results):
+    def ok(self, _):
         return self._msg
 
-    def problem(self, results):
+    def problem(self, _):
         return self._msg
