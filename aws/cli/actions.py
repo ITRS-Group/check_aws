2d0d70239c4721f4371797f606cec1848edf7df0
Robert Wikman <rbw@vault13.org>
2019-10-29 16:05:57 +0100

    Add Management Pack compatible credentials resolver
    This sets credentials_file to its default value if a falsy
    argument was provided, which should make it easier to work
    with management packs and the OP5 Monitor environment in general.
    
    Signed-off-by: Robert Wikman <rbw@vault13.org>

--- aws/cli/actions.py
+++ aws/cli/actions.py
@@ -1,8 +1,10 @@
 from argparse import Action
 
+from aws.consts import Default
+
 
 class DimensionsSerializer(Action):
-    """Performs Dimensions serialization"""
+    """Performs (de)serialization of Dimensions"""
 
     def __call__(self, parser, namespace, value, option_string=None):
         if not value:
@@ -24,3 +26,19 @@ class DimensionsSerializer(Action):
                 raise ValueError("Dimension {} value cannot be empty".format(k))
 
             yield k, v
+
+
+class CredentialsFileResolver(Action):
+    """Credentials file resolver
+
+    Ensures a falsy credentials file argument is set to its field's default value.
+
+    This is mainly to provide compatibility with the various "metadata" packs in OP5 Monitor
+    without using wrapper hacks.
+    """
+
+    def __call__(self, parser, namespace, value, option_string=None):
+        if not value:
+            value = Default.credentials_file.value
+
+        setattr(namespace, self.dest, value)
