ce451761d910846ee9d4457577e58a577dd1f5e0
Robert Wikman <rwikman@op5.com>
2019-11-01 14:25:12 +0100

    Refactor CloudWatchSummary
    This will make the code more readable and
    include metric values in status texts.
    
    Signed-off-by: Robert Wikman <rbw@vault13.org>

--- nagios_aws/summary.py
+++ nagios_aws/summary.py
@@ -5,13 +5,26 @@ from nagios_aws.cli.actions import DimensionsSerializer
 
 class CloudWatchSummary(nagiosplugin.Summary):
     def __init__(self, namespace, metric, dimensions=None):
-        self._msg = "Metric {}:{}".format(namespace, metric)
+        self.namespace = namespace
+        self.metric = metric
+        self.dimensions = dimensions
 
-        if dimensions:
-            self._msg += DimensionsSerializer.dump(dimensions)
+    def get_message(self, result):
+        result_text = result[0].metric.value
 
-    def ok(self, _):
-        return self._msg
+        msg = "Metric {0}:{1} {2}".format(
+            self.namespace,
+            self.metric,
+            result_text
+        )
 
-    def problem(self, _):
-        return self._msg
+        if self.dimensions:
+            msg += DimensionsSerializer.dump(self.dimensions)
+
+        return msg
+
+    def ok(self, result):
+        return self.get_message(result)
+
+    def problem(self, result):
+        return self.get_message(result)
