b7c76c7f1090a524c511460b22057b2d6970fca0
Robert Wikman <rbw@vault13.org>
2021-01-07 13:29:36 +0100

    Update tests

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,5 +1,6 @@
 import pytest
 
+from botocore.exceptions import NoCredentialsError
 from nagios_aws.consts import Default
 
 
@@ -15,30 +16,11 @@ def test_default(cli):
     assert cli().credentials_file == Default.credentials_file.value
 
 
-def test_section_exists(resource):
-    options = resource(
-        overrides={"--credentials": "tests/input/credentials", "--profile": "default"}
-    ).get_credentials()
-
-    assert all([opt for opt in options.values()])
-
-
-def test_non_section(resource):
-    options = resource(
+def test_non_file(resource):
+    # with pytest.raises(NoCredentialsError):
+    resource(
         overrides={
             "--profile": "test_non_section",
-            "--credentials": "tests/input/credentials",
+            "--credentials_file": "tests/input/non_existent_file",
         }
-    ).get_credentials()
-
-    assert not all([opt for opt in options.values()])
-
-
-def test_non_file(resource):
-    with pytest.raises(FileNotFoundError):
-        resource(
-            overrides={
-                "--profile": "test_non_section",
-                "--credentials": "tests/input/non_existent_file",
-            }
-        ).get_credentials()
+    )
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,5 +1,5 @@
 def test_frame(resource):
     res = resource()
 
-    assert res.payload.get("start_time") == res.frame.start
-    assert res.payload.get("end_time") == res.frame.end
+    assert res.payload.get("StartTime") == res.frame.start
+    assert res.payload.get("EndTime") == res.frame.end
