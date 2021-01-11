a2172cb3ef67fec86be2b0b463f8d72fe47fe877
Robert Wikman <rbw@vault13.org>
2021-01-11 09:35:38 +0100

    Fix formatting

--- nagios_aws/cli/options.py
+++ nagios_aws/cli/options.py
@@ -139,7 +139,7 @@ opts = [
             "type": int,
             "default": Default.lag.value,
             "help": "Delay in seconds to add to starting time for gathering metric."
-                    "useful for ec2 basic monitoring which aggregates over 5min periods (default: %(default)s)",
+            "useful for ec2 basic monitoring which aggregates over 5min periods (default: %(default)s)",
         },
     ),
     (
