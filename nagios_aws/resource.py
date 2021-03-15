b0e20687f92e91455445b87b16150faa3e930ec0
Robert Wikman <rbw@vault13.org>
2021-03-15 11:04:03 +0100

    Improve CLI input validation
    This validation is performed by passing given
    Metric+Namespace arguments to the CloudWatch
    `list_metrics` API method. If the combination is
    valid, matched objects with associated dimensions
    are returned.
    
    Validation fails if no objects are returned in the
    response. When this happens, an additional call to
     `list_metrics` is performed with Namespace
     omitted, to find out if the given Metric, or the
     combination of Metric+Namespace is invalid.
    
    If the Metric+Namespace validation call was
    successful, the returned Dimensions are
    consolidated and converted to a set, then compared
     against given Dimensions using issubset().

--- nagios_aws/resource.py
+++ nagios_aws/resource.py
@@ -1,68 +1,145 @@
-from collections import namedtuple
 from datetime import datetime, timedelta
 
 import boto3
 import nagiosplugin
 
 from .consts import NAME
-from .exceptions import UnexpectedResponse
-
-TimeFrame = namedtuple("TimeFrame", ["start", "end"])
+from .exceptions import (
+    InvalidDimension,
+    InvalidMetricNamespace,
+    InvalidMetricType,
+    UnexpectedResponse,
+)
 
 
 class CloudWatchResource(nagiosplugin.Resource):
-    def __init__(self, cfg, *args, **kwargs):
-        self.cfg = cfg
-
-        now = datetime.utcnow()
-
-        self.frame = TimeFrame(
-            start=now - timedelta(seconds=cfg.period + cfg.lag),
-            end=now - timedelta(seconds=cfg.delta),
-        )
-
-        self.payload = dict(
-            Period=self.cfg.period,
-            StartTime=self.frame.start,
-            EndTime=self.frame.end,
-            MetricName=self.cfg.metric,
-            Namespace=self.cfg.namespace,
-            Statistics=(self.cfg.statistic,),
-            Dimensions=self.cfg.dimensions,
-            Unit=self.cfg.unit,
-        )
-
-        self._session = boto3.Session(region_name=self.cfg.region)
-
+    def __init__(self, cmdargs, *args, **kwargs):
+        self.cmdargs = cmdargs
+        self.session = boto3.Session(region_name=cmdargs.region)
         super(CloudWatchResource, self).__init__(*args, **kwargs)
 
-    @property
-    def connection(self):
-        return self._session.client("cloudwatch")
+    def _query_make(self, cmdargs, validate=True):
+        """Transforms cmdargs to a metric statistics query
 
-    def _send(self, *args, **kwargs):
-        return self.connection.get_metric_statistics(*args, **self.payload, **kwargs)
+        Performs validation of Metric + Namespace, and Dimensions if
+        validation is set to True.
 
-    def probe(self):
-        """Generator for yielding metrics from Datapoints"""
+        Args:
+            cmdargs: Argparse object
+            validate: Validate query
 
-        response = self._send()
-        if "Datapoints" not in response or len(response["Datapoints"]) < 1:
-            return []
+        Returns:
+            Metric statistics query
+        """
 
-        # Make statistic input case insensitive
-        stat_key = self.cfg.statistic.capitalize()
-        label = response["Label"]
+        now = datetime.utcnow()
+        query = dict(
+            Period=cmdargs.period,
+            StartTime=now - timedelta(seconds=cmdargs.period + cmdargs.lag),
+            EndTime=now - timedelta(seconds=cmdargs.delta),
+            MetricName=cmdargs.metric,
+            Namespace=cmdargs.namespace,
+            Statistics=(cmdargs.statistic,),
+            Dimensions=cmdargs.dimensions,
+            Unit=cmdargs.unit,
+        )
 
-        if label != self.cfg.metric:
-            raise UnexpectedResponse(
-                f"Unexpected Metric in Response. Got: {label}, Expected: {self.cfg.metric}"
+        if validate is True:
+            self._query_validate(query)
+
+        return query
+
+    def _metrics_get(self, **kwargs):
+        """Get a list of available cloudwatch metrics including dimensions
+
+        Args:
+            **kwargs: Conditions to pass along to list_metrics()
+
+        Returns:
+            Response content
+        """
+
+        return self.session.client("cloudwatch").list_metrics(**kwargs)
+
+    def _query_validate(self, query):
+        """Validate the given query
+
+        Raises an exception if Metric, Namespace or Dimensions in the query is invalid.
+
+        Raises:
+            InvalidDimension: Invalid Dimensions
+            InvalidMetricType: Invalid Metric
+            InvalidMetricNamespace: No such Metric in Namespace
+
+        Returns:
+            True
+        """
+
+        namespace, metric = query["Namespace"], query["MetricName"]
+        avail_dimensions = []
+        # Convert dimensions in query to a set of key-value pairs, for comparison with `dimensions`
+        req_dimensions = set(
+            [(d["Name"], d["Value"]) for d in query.get("Dimensions", ()) if d]
+        )
+
+        response = self._metrics_get(Namespace=namespace, MetricName=metric)
+        metrics = response.get("Metrics")
+
+        # Create set of available dimensions from `dimension` items in the metrics object
+        for m in metrics:
+            avail_dimensions.extend(
+                [(d["Name"], d["Value"]) for d in m["Dimensions"] if d]
             )
 
-        for point in response["Datapoints"]:
-            stat_val = point.get(stat_key)
+        # Perform validation
+        if not metrics:
+            # Figure out what went wrong
+            response = self._metrics_get(MetricName=metric)
+            if response["Metrics"]:  # Does the given metric exist?
+                raise InvalidMetricNamespace(
+                    f"No metric of type '{metric}' in Namespace '{namespace}'"
+                )
+            else:  # Invalid metric type
+                raise InvalidMetricType(f"No such metric type: {metric}")
+        elif not req_dimensions.issubset(set(avail_dimensions)):
+            raise InvalidDimension(
+                f"No Metrics of type '{metric}' with Dimensions '{req_dimensions}'"
+            )
 
-            yield nagiosplugin.Metric(self.cfg.metric, stat_val)
+        return True
+
+    def _statistics_get(self):
+        """Get metric statistics data
+
+        Returns:
+            Metric object with a list of Datapoints
+        """
+
+        query = self._query_make(self.cmdargs)
+        return self.session.client("cloudwatch").get_metric_statistics(**query)
+
+    def probe(self):
+        """Query AWS CloudWatch for health data
+
+        Returns:
+            generator yielding nagiosplugin.Metric objects
+        """
+
+        response = self._statistics_get()
+
+        if "Datapoints" not in response or not response["Datapoints"]:
+            return []
+
+        label = response["Label"]
+        if label != self.cmdargs.metric:
+            raise UnexpectedResponse(
+                f"Unexpected Metric in Response. Got: {label}, Expected: {self.cmdargs.metric}"
+            )
+
+        stat_name = self.cmdargs.statistic.capitalize()
+        for point in response["Datapoints"]:
+            stat_val = point.get(stat_name)
+            yield nagiosplugin.Metric(self.cmdargs.metric, stat_val)
 
     @property
     def name(self):
