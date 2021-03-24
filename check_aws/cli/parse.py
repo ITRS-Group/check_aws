bb582b96527c145d9c3b6d76c6f2690e22961432
Robert Wikman <rbw@vault13.org>
2021-03-24 12:55:50 +0100

    Move cli.parse_cmdline to cli.parse.parse_cmdline

---
+++ check_aws/cli/parse.py
@@ -0,0 +1,20 @@
+import argparse
+
+from ..consts import UNITS
+from .options import opts
+
+
+def parse_cmdline(args):
+    parser = argparse.ArgumentParser(
+        description="Plugin for monitoring CloudWatch-enabled AWS instances"
+    )
+
+    for opt, conf in opts:
+        parser.add_argument(*opt, **conf)
+
+    parsed = parser.parse_args(args)
+
+    if parsed.unit and parsed.unit not in UNITS:
+        raise ValueError("Unit must be one of: {}".format(UNITS))
+
+    return parsed
