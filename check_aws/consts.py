74fa732367117ae5b9f9be39215ab0c595a84feb
Robert Wikman <rbw@vault13.org>
2021-03-29 08:42:53 +0200

    Move consts.InputDefault => cli.options.CommandArguments

--- check_aws/consts.py
+++ check_aws/consts.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 NAME = "AWS"
 STATISTICS = ["Average", "Sum", "SampleCount", "Maximum", "Minimum"]
 UNITS = [
@@ -31,23 +29,3 @@ UNITS = [
     "Terabits/Second",
     "Count/Second",
 ]
-
-
-# @TODO - move to cli.options
-# @TODO - rename to CommandArguments
-@dataclass
-class InputDefault:
-    namespace: str = ""
-    metric: str = ""
-    unit: str = None
-    region: str = ""
-    credentials_file: str = ""
-    profile: str = "default"
-    statistic: str = "Average"
-    dimensions: tuple = ()
-    period: int = 60
-    lag: int = 0
-    warning: int = 0
-    critical: int = 0
-    verbosity: int = 0
-    delta: int = 0
